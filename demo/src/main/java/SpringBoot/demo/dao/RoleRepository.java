d38e16e6954900492725c80fd014864e49a1608d
Vibl-pass <62307359+Vibl-pass@users.noreply.github.com>
2020-11-26 18:05:46 +0300

    3.1.1.v2

--- demo/src/main/java/SpringBoot/demo/dao/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package SpringBoot.demo.dao;
-
-import SpringBoot.demo.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRole(String name);
-}
